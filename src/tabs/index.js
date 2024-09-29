de92e988510a1e73bf2e14ec620872d289173cac
Mr.Rattlesnake <vuong.sky101@gmail.com>
2024-09-29 17:34:24 +0700

    Hinh co the thu phong

--- src/tabs/index.js
+++ src/tabs/index.js
@@ -1,4 +1,4 @@
-import IndexRouter from '../homeRouters/indexRouter'
+import IndexRouter from '../routers/indexRouter'
 import EventTab from './eventTab';
 import PersonTab from './personTab';
 import NotiTab from './notiTab';
